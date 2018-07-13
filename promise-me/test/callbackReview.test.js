af21198d7de3b1b31dd520c58c6885a49c186017
Kin Chan <xkinchanx@gmail.com>
2018-07-13 12:25:52 -0700

    [refactor] filePath modularity

--- promise-me/test/callbackReview.test.js
+++ promise-me/test/callbackReview.test.js
@@ -2,9 +2,11 @@
 const fs = require('fs')
 const { createMyFile, readMyFile, readFileAndConvertToSentence } = require('../solutions/callbackSolutions.js');
 
+const filePath = __dirname + '/lib/callbackFile.txt';
+
 describe('functions should would with via callback style', () => {
-  beforeEach(() => {
-    fs.unlink(__dirname + '/lib/callbackFile.txt', (err) => {
+  afterEach(() => {
+    fs.unlink(filePath, (err) => {
       if (err) { return }
       console.log('callbackFile.txt was deleted')
     })
@@ -19,21 +21,21 @@ describe('functions should would with via callback style', () => {
   })
   
   test('has a callback that takes an something not null or undefined', (done) => {
-    createMyFile(__dirname + '/lib/callbackFile.txt', 'hello', () => {
-      readMyFile(__dirname + '/lib/callbackFile.txt', (err, content) => {
+    createMyFile(filePath, 'hello', () => {
+      readMyFile(filePath, (err, content) => {
         expect(err).toBeNull();
         expect(content).toEqual('hello')
         done()
       })
     })
   })
-})
-
-test('has a callback that takes an something not null or undefined', (done) => {
-  createMyFile(__dirname + '/lib/callbackFile.txt', 'hello\nyou are\nsuper', () => {
-    readFileAndConvertToSentence(__dirname + '/lib/callbackFile.txt', (err, content) => {
-      expect(content).toEqual('hello you are super')
-      done()
+  
+  test('has a callback that takes an something not null or undefined', (done) => {
+    createMyFile(filePath, 'hello\nyou are\nsuper', () => {
+      readFileAndConvertToSentence(filePath, (err, content) => {
+        expect(content).toEqual('hello you are super')
+        done()
+      })
     })
   })
 })
