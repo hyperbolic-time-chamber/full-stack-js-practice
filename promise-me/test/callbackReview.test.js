34d0715a171f44bcad2e17506d79d9ab570bba27
Kin Chan <xkinchanx@gmail.com>
2018-07-13 00:20:02 -0700

    [test] readFile callback should return correct content as second argument

--- promise-me/test/callbackReview.test.js
+++ promise-me/test/callbackReview.test.js
@@ -2,9 +2,24 @@
 const fs = require('fs')
 const { createMyFile, readMyFile } = require('../solutions/callbackSolutions.js');
 
+beforeEach(() => {
+  fs.unlink(__dirname + '/lib/callbackFile.txt', (err) => {
+    if (err) { return }
+    console.log('callbackFile.txt was deleted')
+  })
+})
+
 test('has a callback that takes an error as its first argument', () => {
   readMyFile('./test', (err, content) => {
     expect(err.code).toEqual('EISDIR');
     expect(content).toBeUndefined();
+  })
+})
+
+test('has a callback that takes an something not null or undefined', () => {
+  createMyFile(__dirname + '/lib/callbackFile.txt', 'hello', () => {
+    readMyFile(__dirname + '/lib/callbackFile.txt', (err, content) => {
+      expect(content).toEqual('hello')
+    })
   })
 })
