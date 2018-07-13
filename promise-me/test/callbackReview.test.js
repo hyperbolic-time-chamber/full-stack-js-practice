269efe333918e6b1861d2c975d8ac8e77f8b3822
Kin Chan <xkinchanx@gmail.com>
2018-07-13 11:47:06 -0700

    [test] for callbackReview functions

--- promise-me/test/callbackReview.test.js
+++ promise-me/test/callbackReview.test.js
@@ -1,25 +1,39 @@
 // const { someSortOfFunction } = require('../exercises/callbackReview');
 const fs = require('fs')
-const { createMyFile, readMyFile } = require('../solutions/callbackSolutions.js');
+const { createMyFile, readMyFile, readFileAndConvertToSentence } = require('../solutions/callbackSolutions.js');
 
-beforeEach(() => {
-  fs.unlink(__dirname + '/lib/callbackFile.txt', (err) => {
-    if (err) { return }
-    console.log('callbackFile.txt was deleted')
+describe('functions should would with via callback style', () => {
+  beforeEach(() => {
+    fs.unlink(__dirname + '/lib/callbackFile.txt', (err) => {
+      if (err) { return }
+      console.log('callbackFile.txt was deleted')
+    })
+  })
+  
+  test('has a callback that takes an error as its first argument', (done) => {
+    readMyFile('./test', (err, content) => {
+      expect(err.code).toEqual('EISDIR');
+      expect(content).toBeNull();
+      done();
+    })
+  })
+  
+  test('has a callback that takes an something not null or undefined', (done) => {
+    createMyFile(__dirname + '/lib/callbackFile.txt', 'hello', () => {
+      readMyFile(__dirname + '/lib/callbackFile.txt', (err, content) => {
+        expect(err).toBeNull();
+        expect(content).toEqual('hello')
+        done()
+      })
+    })
   })
 })
 
-test('has a callback that takes an error as its first argument', () => {
-  readMyFile('./test', (err, content) => {
-    expect(err.code).toEqual('EISDIR');
-    expect(content).toBeUndefined();
-  })
-})
-
-test('has a callback that takes an something not null or undefined', () => {
-  createMyFile(__dirname + '/lib/callbackFile.txt', 'hello', () => {
-    readMyFile(__dirname + '/lib/callbackFile.txt', (err, content) => {
-      expect(content).toEqual('hello')
+test('has a callback that takes an something not null or undefined', (done) => {
+  createMyFile(__dirname + '/lib/callbackFile.txt', 'hello\nyou are\nsuper', () => {
+    readFileAndConvertToSentence(__dirname + '/lib/callbackFile.txt', (err, content) => {
+      expect(content).toEqual('hello you are super')
+      done()
     })
   })
 })
