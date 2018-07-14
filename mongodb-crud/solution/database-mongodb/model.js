246b6a57f08fe1a770b3f216bf127bb05a6da16b
Daniel Lai <dlai813@gmail.com>
2018-07-13 21:53:31 -0700

    remove model in solution directory

--- mongodb-crud/solution/database-mongodb/model.js
+++ mongodb-crud/solution/database-mongodb/model.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 
 const toDoListSchema = new Schema({
   name: { type: String, maxlength: 50, required: true, unique: true },
-  is_urgent: { type: Boolean, required: true },
 });
 
 const ToDoList = db.model('ToDoList', toDoListSchema);
