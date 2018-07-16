a022cdb9875515c5bda22592ae996c1d22051e92
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-07-15 22:53:06 -0700

    [prog] add starter files and instructions

---
+++ 2-mysql-crud/server/sequelize/controller.js
@@ -0,0 +1,17 @@
+/* eslint-disable prefer-arrow-callback, no-undef, func-names, no-var, object-shorthand  */
+
+var { Todo } = require('../../database-mysql-sequelize/models');
+
+module.exports = {
+  /* Get all todos. */
+  read: function(req, res) {},
+
+  /* Create one todo. */
+  create: function(req, res) {},
+
+  /* Update one todo. */
+  update: function(req, res) {},
+
+  /* Delete one todo. */
+  delete: function(req, res) {},
+};
